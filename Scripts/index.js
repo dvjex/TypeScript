8d14d5bf0fca1990b6617fb180498cbab3a64c2e
dvjex <digmvijay@gmail.com>
2017-05-03 23:18:18 -0400

    Basic example

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,1 +1,20 @@
 console.log('Hello');
+
+var Animal = (function () {
+    function Animal(name, legs) {
+        this.name = name;
+        this.legs = legs;
+    }
+    Animal.prototype.showInfo = function () {
+        console.log('Name: ' + this.name + '\nNumber of legs: ' + this.legs);
+    };
+    return Animal;
+})();
+
+window.onload = function () {
+    var penguin = new Animal('Penguin', 2);
+    penguin.showInfo();
+
+    var dog = new Animal('Dog', 4);
+    dog.showInfo();
+};
--- Scripts/index.ts
+++ Scripts/index.ts
@@ -1,1 +1,23 @@
 console.log('Hello');
+
+class Animal{
+	legs: number;
+	name: string;
+	
+	constructor(name: string, legs: number){
+		this.name = name;
+		this.legs = legs;
+	}
+	
+	showInfo(){
+		console.log('Name: '+this.name+'\nNumber of legs: '+this.legs);
+	}
+}
+
+window.onload = function(){
+	var penguin = new Animal('Penguin', 2);
+	penguin.showInfo();
+	
+	var dog = new Animal('Dog',4);
+	dog.showInfo();
+};
