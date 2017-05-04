8d32c526106107d5e95d12dcc7eb9f37d8a891ff
dvjex <digmvijay@gmail.com>
2017-05-03 23:25:19 -0400

    Inheritance example

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,3 +1,9 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
 console.log('Hello');
 
 var Animal = (function () {
@@ -11,10 +17,29 @@ var Animal = (function () {
     return Animal;
 })();
 
+var TwoLegs = (function (_super) {
+    __extends(TwoLegs, _super);
+    function TwoLegs(name) {
+        _super.call(this, name, 2);
+    }
+    return TwoLegs;
+})(Animal);
+var FourLegs = (function (_super) {
+    __extends(FourLegs, _super);
+    function FourLegs(name) {
+        _super.call(this, name, 4);
+    }
+    return FourLegs;
+})(Animal);
+
 window.onload = function () {
     var penguin = new Animal('Penguin', 2);
     penguin.showInfo();
+    var ping = new TwoLegs('Ping the Penguin');
+    ping.showInfo();
 
     var dog = new Animal('Dog', 4);
     dog.showInfo();
+    var cat = new FourLegs('Billy Cat');
+    cat.showInfo();
 };
--- Scripts/index.ts
+++ Scripts/index.ts
@@ -14,10 +14,25 @@ class Animal{
 	}
 }
 
+class TwoLegs extends Animal {
+	constructor(name: string){
+		super(name, 2);
+	}
+}
+class FourLegs extends Animal {
+	constructor(name: string){
+		super(name, 4);
+	}
+}
+
 window.onload = function(){
 	var penguin = new Animal('Penguin', 2);
 	penguin.showInfo();
+	var ping = new TwoLegs('Ping the Penguin');
+	ping.showInfo();
 	
 	var dog = new Animal('Dog',4);
 	dog.showInfo();
+	var cat = new FourLegs('Billy Cat');
+	cat.showInfo();
 };
