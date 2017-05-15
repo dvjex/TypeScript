ccf39f1f892e1bfd0d644e836a18283cae50c09b
dvjex <digmvijay@gmail.com>
2017-05-14 23:56:52 -0400

    Log Sample

---
+++ Succintly/Web/Scripts/sample.js
@@ -0,0 +1,11 @@
+﻿function log(message) {
+    if (typeof window.console !== 'undefined') {
+        window.console.log(message);
+    }
+}
+var testLog = 'Hello';
+log(testLog);
+
+var b = 'fd';
+b = 1;
+//# sourceMappingURL=sample.js.map
---
+++ Succintly/Web/Scripts/sample.ts
@@ -0,0 +1,10 @@
+﻿function log(message: string): void {
+    if (typeof window.console !== 'undefined') {
+        window.console.log(message);
+    }
+}
+var testLog = 'Hello';
+log(testLog);
+
+var b = 'fd';
+b = 1;
