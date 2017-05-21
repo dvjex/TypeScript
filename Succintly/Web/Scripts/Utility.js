6195873e24a827cd0384a17355ad465efe877e72
dvjex <digmvijay@gmail.com>
2017-05-20 20:38:29 -0400

    Added Utility module with Logging

---
+++ Succintly/Web/Scripts/Utility.js
@@ -0,0 +1,18 @@
+﻿var Succintly;
+(function (Succintly) {
+    var Utility;
+    (function (Utility) {
+        var Logger = (function () {
+            function Logger() {
+            }
+            Logger.prototype.log = function (message) {
+                if (typeof window.console !== 'undefined') {
+                    window.console.log(message);
+                }
+            };
+            return Logger;
+        })();
+        Utility.Logger = Logger;
+    })(Utility || (Utility = {}));
+})(Succintly || (Succintly = {}));
+//# sourceMappingURL=Utility.js.map
---
+++ Succintly/Web/Scripts/Utility.ts
@@ -0,0 +1,11 @@
+﻿module Succintly {
+    module Utility {
+        export class Logger {
+            log(message: string): void {
+                if (typeof window.console !== 'undefined') {
+                    window.console.log(message);
+                }
+            }
+        }
+    }
+} 
