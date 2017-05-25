5c7b9a14882cfc5badbd284d96fe130a41787372
dvjex <digmvijay@gmail.com>
2017-05-25 01:52:38 -0400

    Unit testing TypeScript with tsUnit Initial commit

---
+++ Succintly/Web/Scripts/Calculations.js
@@ -0,0 +1,20 @@
+﻿var Succintly;
+(function (Succintly) {
+    var Math = (function () {
+        function Math() {
+        }
+        Math.prototype.addTwoNumbers = function (a, b) {
+            return a + b;
+        };
+
+        Math.prototype.addNumbers = function (num) {
+            var sum = 0;
+            for (var i = 0; i < num.length; i++)
+                sum = sum + num[i];
+            return sum;
+        };
+        return Math;
+    })();
+    Succintly.Math = Math;
+})(Succintly || (Succintly = {}));
+//# sourceMappingURL=Calculations.js.map
---
+++ Succintly/Web/Scripts/Calculations.ts
@@ -0,0 +1,14 @@
+﻿module Succintly {
+    export class Math {
+        addTwoNumbers(a: number, b: number): number {
+            return a + b;
+        }
+
+        addNumbers(num: number[]): number {
+            var sum: number = 0;
+            for (var i = 0; i < num.length; i++)
+                sum = sum + num[i];
+            return sum;
+        }
+    }
+} 
